631dbba6cac18727544e87ed911a5487aa80e979
jneilliii <jneilliii+github@gmail.com>
2023-09-27 23:52:42 -0400

    1.0.3
    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_prusaslicerthumbnails"
 plugin_name = "Slicer Thumbnails"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.3rc3"
+plugin_version = "1.0.3"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
